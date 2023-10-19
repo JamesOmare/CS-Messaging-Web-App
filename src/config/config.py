168468fcdeb87ffc701e5da40bdaba542f40d4dc
JamesOmare <jamesomare922@gmail.com>
2023-10-19 08:52:32 +0300

    added project structure i.e models, views, templates, static, config, auth folders

---
+++ src/config/config.py
@@ -0,0 +1,10 @@
+from decouple import config
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = config('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = config('DATABASE_URL')
+   
